c7c290cf1a159c3fc02729f52bbbeb6f455acad2
Loembe edmond <edmond@MacBook-Air-de-Edmond.local>
2025-03-07 10:51:37 +0100

    [ADD] component Solution

--- website_next/jest.setup.js
+++ website_next/jest.setup.js
@@ -1,1 +1,14 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    };
+  };
--- website_next/src/components/Home/HomeUnlogged.jsx
+++ website_next/src/components/Home/HomeUnlogged.jsx
@@ -5,6 +5,7 @@ import Authentification from '../Authentification';
 import Team from '@/components/Team/index';
 import Header from '@/components/Header/Header';
 import About from '@/components/About/About';
+import Solution from '@/components/Solution/Solution';
 import ArrowSeparatorLeft from '@/components/_ui/Arrow/ArrowSeparatorLeft';
 import ArrowSeparatorRight from '@/components/_ui/Arrow/ArrowSeparatorRight';
 
@@ -16,6 +17,8 @@ export default function HomeUnlogged() {
         <ArrowSeparatorLeft />
         <About />
         <ArrowSeparatorRight />
+        <Solution />
+        <ArrowSeparatorLeft />
         <Authentification />
         <Team />
     </div>
